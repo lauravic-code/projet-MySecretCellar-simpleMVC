c175e00e07404dd3eb24eedd761f1658a99f2692
Karim <kouach2002@hotmail.com>
2022-10-27 10:39:46 +0200

    change avatar name

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -4,8 +4,8 @@ namespace App\Controller;
 
 class RegisterController extends AbstractController
 {
-    public function viewRegister(){
+    public function viewRegister()
+    {
         return $this->twig->render('register/register.html.twig');
-
     }
 }
