2ba953d055a34628f8e614d53df50b9b1b32f35f
Karim <kouach2002@hotmail.com>
2022-10-27 10:34:54 +0200

    sign in page/ start register page

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controller;
+
+class RegisterController extends AbstractController
+{
+    public function viewRegister(){
+        return $this->twig->render('register/register.html.twig');
+
+    }
+}
