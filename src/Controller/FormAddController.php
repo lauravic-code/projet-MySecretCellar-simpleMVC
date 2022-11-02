b22ed12548e4ab83adfbe19039643b3b2038ed4d
laura vicente <lauravic.pro@gmail.com>
2022-11-02 12:27:16 +0100

    changed form.html.twig to AddForm.html +  change routes

--- src/Controller/FormAddController.php
+++ src/Controller/FormAddController.php
@@ -6,10 +6,10 @@ namespace App\Controller;
 
 class FormController extends AbstractController
 {
-    public function viewForm(): string
+    public function viewAddForm(): string
     {
         // $formManager = new FormManager();
 
-        return $this->twig->render('Form/form.html.twig');
+        return $this->twig->render('Form/AddForm.html.twig');
     }
 }
