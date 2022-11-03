9db8544003d3e11dc006e89ebbd255e1c971ac58
Valerie <valerie.ielsch@gmail.com>
2022-11-03 17:24:28 +0100

    managers created(for filters and forme) to use abstractMAnager to access selectAll methods

---
+++ src/Model/GrapeVarietyManager.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Model;
+
+use PDO;
+
+class GrapeVarietyManager extends AbstractManager
+{
+    public const TABLE = 'grapeVariety';
+}
---
+++ src/Model/WinePairingManager.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Model;
+
+use PDO;
+
+class WinePairingManager extends AbstractManager
+{
+    public const TABLE = 'winePairing';
+}
